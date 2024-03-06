09cba06a9b0ee07226205f4a2b5d8e2648c1f896
leo <waihinkeung@gmail.com>
2024-03-06 16:54:30 +0800

    update

--- README.md
+++ README.md
@@ -9,10 +9,26 @@ https://api-ninjas.com/api/facts
 https://api-ninjas.com/api/randomword
 
 Project:
+//Fetch pokemon API to get icons
 //Choose one Pokemon from 3-5?
+
 //Start game by press'space'
 //Press 'space' to jump
+//Press up, icon face-up
+//Press release, icon face-return
+
+//Pokemon axis-x remain unchange, only axis-y change when press 'space'
+//If press nothing, Pokemon fall down in gravity
+//Pokemon has a max-height
+
+//Pipe create random
+//Pipe-top -> opening <- Pipe-down
+//After screen, pipe disappear
+
 //Hit pipe
+//Fall down to bottom
 //Game over
-//Show 'facts'
-//Resatrt button
+
+//Fetch'facts' API to show facts
+//Chose another pokemon button
+//Resatrt with same pokemon button
---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setGame();
+});
+
+function menu() {}
+function fetchP() {
+  //Fetch pokemon API to get icons
+}
+function chooseP() {
+  //Choose one Pokemon from 3-5?
+}
+
+function moveP() {
+  //Start game by press'space'
+  //Press 'space' to jump
+  //Press up, icon face-up
+  //Press release, icon face-return
+  //Pokemon axis-x remain unchange, only axis-y change when press 'space'
+  //If press nothing, Pokemon fall down in gravity
+  //Pokemon has a max-height
+}
+
+function createPipe() {
+  //Pipe create random
+  //Pipe-top -> opening <- Pipe-down
+  //After screen, pipe disappear
+}
+
+function advanceMode() {
+  //if score to some points, next level. Change background / Pokemon / Pipe / Speed / Opening
+}
+
+function cheatMode() {
+  //No pipe mode
+}
+
+function gameover() {
+  //Hit pipe
+  //Fall down to bottom
+  //Game over
+}
+
+function afterGame() {
+  //Fetch'facts' API to show facts
+  //Return menu button
+  //Chose another pokemon button
+  //Resatrt with same pokemon button
+}
